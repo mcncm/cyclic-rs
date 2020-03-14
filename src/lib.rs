c24ab3e45f4f73b9308f83ca810be60164314472
mcncm <33966432+mcncm@users.noreply.github.com>
2020-03-14 16:32:53 -0400

    Added pow, div, and compile-time primality checking
    * Because there are not yet `where` clauses for const generic parameters, I
      implemented `div` such that it panics if it is ever called on a `Modular::<N>`
      of composite modulus. This runtime failure is almost as good as a compile-time
      check, provided we don't need to check the primality of `N` on every call.
    * I achieved this constant-time overhead by testing for primality with a `const
      fn`. This in turn requires some compromise--because of limitations in `const
      fn`, we test for primality using a naive recursive function. Therefore, moduli
      are currently limited by the maximum number of stack frames the compiler
      allows.
    * `pow` method added in the main `impl` block. This is completely unoptimized,
      and will overflow for moderately-sized exponents.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(incomplete_features)]
 #![feature(const_generics)]
+#![feature(const_fn)]
+#![feature(const_if_match)]
 
 pub type Int = u32;
 
@@ -10,18 +12,31 @@ pub type Int = u32;
 ///   wrapping for large-enough `N`, and small exponentials might be best
 ///   implemented by repeated multiplication.
 ///
-/// * Wrapping ranges should be added.
+/// * Wrapping ranges should be added (This doesn't seem to be possible using
+///   the built-in `std::ops::Range`, which expects a type implementing
+///   `PartialOrd`)
+///
+/// * Study optimizations from theory of finite groups, and apply some of them!
 mod modular {
+    use super::primes::is_prime;
     use super::Int;
     use std::cmp::PartialEq;
-    use std::ops::{Add, Mul, Neg, Sub};
+    use std::ops::{Add, Div, Mul, Neg, Sub};
 
-    #[derive(Debug)]
+    #[derive(Debug, Clone, Copy)]
     pub struct Modular<const N: Int>(Int);
 
     impl<const N: Int> Modular<{ N }> {
+        const PRIMALITY: bool = is_prime({ N });
+
         pub fn new(val: Int) -> Self {
             Self { 0: val % { N } }
+        }
+
+        /// Raises `self` to the power of `exp`, using the underlying integral
+        /// type's `pow` method.
+        pub fn pow(self, exp: u32) -> Self {
+            Self::new(self.0.pow(exp))
         }
     }
 
@@ -37,7 +52,7 @@ mod modular {
         type Output = Self;
 
         fn add(self, other: Self) -> Self::Output {
-            Self::new((self.0 + other.0) % { N })
+            Self::new(self.0 + other.0)
         }
     }
 
@@ -45,7 +60,7 @@ mod modular {
         type Output = Self;
 
         fn mul(self, other: Self) -> Self::Output {
-            Self::new((self.0 * other.0) % { N })
+            Self::new(self.0 * other.0)
         }
     }
 
@@ -57,10 +72,126 @@ mod modular {
         }
     }
 
+    /// Division for prime modulus. This currently fails at runtime for
+    /// composite `N`; as const generics are stabilized I hope to promote this
+    /// to a compile-time check with a `where is_prime({ N })` clause.
+    impl<const N: Int> Div for Modular<{ N }> {
+        type Output = Self;
+        fn div(self, other: Self) -> Self::Output {
+            assert!(Self::PRIMALITY);
+            // Maximally naive implementation. Replace with the extended
+            // euclidean division algorithm.
+            let mut inv: Int = 1;
+            for n in 1..({ N }) {
+                if Self::new(other.0) * Self::new(n) == Self::new(1) {
+                    inv = n;
+                }
+            }
+            self * Self::new(inv)
+        }
+    }
+
     impl<const N: Int> PartialEq for Modular<{ N }> {
         fn eq(&self, other: &Self) -> bool {
             self.0 == other.0
         }
+    }
+}
+
+mod primes {
+    use super::Int;
+
+    /// Checks that all values less than or equal to `inf`, other than 1, are
+    /// coprime to `n`, and returns `true` if this is the case.
+    const fn inferior_values_coprime(n: Int, inf: Int) -> bool {
+        // This function is necessary--must be written in the following unusual
+        // style--because of constraints imposed by `const fn` in the current
+        // rustc release.
+        //
+        // Because of the unfortunate limitation of using recursive calls for
+        // compile-time primality testing, it would be very nice if I could
+        // optimize away some of these calls. It's likely that a better
+        // algorithm could remove this burden.
+        match inf {
+            1 => true,
+            _ => match n % inf {
+                0 => false,
+                _ => inferior_values_coprime(n, inf - 1),
+            },
+        }
+    }
+
+    #[test]
+    fn inferior_values_coprime_ok() {
+        // Primes should test coprime to all 1 < n < p
+        assert!(inferior_values_coprime(3, 2));
+        assert!(inferior_values_coprime(5, 4));
+        assert!(inferior_values_coprime(7, 6));
+
+        // Composites should not!
+        assert!(!inferior_values_coprime(6, 5));
+
+        // But this should pass for composite numbers on values less than their
+        // least divisor
+        assert!(inferior_values_coprime(35, 4));
+    }
+
+    const fn bit_len(n: Int) -> u32 {
+        let int_size = Int::max_value().count_ones();
+        int_size - n.leading_zeros()
+    }
+
+    #[test]
+    fn bit_len_ok() {
+        assert_eq!(bit_len(0), 0);
+        assert_eq!(bit_len(1), 1);
+        assert_eq!(bit_len(2), 2);
+        assert_eq!(bit_len(3), 2);
+        assert_eq!(bit_len(4), 3);
+    }
+
+    /// A compile=time primality checker. This users a naive and inefficient
+    /// primality test that checks divisbility by all integer values up to the
+    /// square root of N.
+    pub const fn is_prime(n: Int) -> bool {
+        // This function must be written in the following unusual style because
+        // of constraints imposed by `const fn` in the current rustc release.
+        // Unfortunately, this means that `N` is limited by the default maximum
+        // stack size.
+        match n {
+            0 | 1 => false,
+            _ => {
+                let bit_len_limit = 2 << (bit_len(n) >> 1);
+                let test_limit = match bit_len_limit >= n {
+                    true => n - 1,
+                    false => bit_len_limit,
+                };
+                inferior_values_coprime(n, test_limit)
+            }
+        }
+    }
+
+    #[test]
+    fn is_prime_ok() {
+        // primes should test correctly
+        assert!(is_prime(3));
+        assert!(is_prime(5));
+        assert!(is_prime(7));
+        assert!(is_prime(11));
+        assert!(is_prime(13));
+        assert!(is_prime(17));
+
+        // 0 and 1 should fail
+        assert!(!is_prime(0));
+        assert!(!is_prime(1));
+
+        // ...and so should composites.
+        assert!(!is_prime(4));
+        assert!(!is_prime(6));
+        assert!(!is_prime(8));
+        assert!(!is_prime(9));
+        assert!(!is_prime(10));
+        assert!(!is_prime(12));
     }
 }
 
@@ -73,7 +204,7 @@ mod tests {
 
     #[test]
     fn residue_class_equality() {
-        for v in 0..=MODULUS {
+        for v in 0..MODULUS {
             let m = Modular::<MODULUS>::new(v);
             for n in 0..=4 {
                 assert_eq!(m, Modular::<MODULUS>::new(v + n * MODULUS));
@@ -82,17 +213,17 @@ mod tests {
     }
 
     #[test]
-    fn neg_correct() {
-        for v in 0..=MODULUS {
+    fn neg_ok() {
+        for v in 0..MODULUS {
             let m = Modular::<MODULUS>::new(v);
             assert_eq!(-m, Modular::<MODULUS>::new(MODULUS - v));
         }
     }
 
     #[test]
-    fn add_correct() {
-        for v1 in 0..=MODULUS {
-            for v2 in 0..=MODULUS {
+    fn add_ok() {
+        for v1 in 0..MODULUS {
+            for v2 in 0..MODULUS {
                 let m1 = Modular::<MODULUS>::new(v1);
                 let m2 = Modular::<MODULUS>::new(v2);
                 assert_eq!(m1 + m2, Modular::<MODULUS>::new(v1 + v2));
@@ -101,9 +232,9 @@ mod tests {
     }
 
     #[test]
-    fn mul_correct() {
-        for v1 in 0..=MODULUS {
-            for v2 in 0..=MODULUS {
+    fn mul_ok() {
+        for v1 in 0..MODULUS {
+            for v2 in 0..MODULUS {
                 let m1 = Modular::<MODULUS>::new(v1);
                 let m2 = Modular::<MODULUS>::new(v2);
                 assert_eq!(m1 * m2, Modular::<MODULUS>::new(v1 * v2));
@@ -112,15 +243,45 @@ mod tests {
     }
 
     #[test]
-    fn sub_correct() {
-        for v1 in 0..=MODULUS {
-            for v2 in 0..=MODULUS {
+    fn sub_ok() {
+        for v1 in 0..MODULUS {
+            for v2 in 0..MODULUS {
                 let m1 = Modular::<MODULUS>::new(v1);
                 let m2 = Modular::<MODULUS>::new(v2);
                 assert_eq!(
                     m1 - m2,
                     Modular::<MODULUS>::new(v1) + (-Modular::<MODULUS>::new(v2))
                 );
+            }
+        }
+    }
+
+    #[test]
+    fn div_ok() {
+        for v1 in 1..MODULUS {
+            for v2 in 1..MODULUS {
+                let m1 = Modular::<MODULUS>::new(v1);
+                let m2 = Modular::<MODULUS>::new(v2);
+                assert_eq!((m1 / m2) * m2, m1,);
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn div_fail_composite() {
+        const COMPOSITE: Int = 6;
+        let m1 = Modular::<COMPOSITE>::new(4);
+        let m2 = Modular::<COMPOSITE>::new(2);
+        let _m = m1 / m2;
+    }
+
+    #[test]
+    fn pow_ok() {
+        for v in 0..=MODULUS {
+            for exp in 0..=10 {
+                let m = Modular::<MODULUS>::new(v);
+                assert_eq!(m.pow(exp), Modular::<MODULUS>::new(v.pow(exp)));
             }
         }
     }
